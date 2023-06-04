e4cf882988e8f89b7f18b6534722ea57a58997b6
Aleksandra Sobiecka <aleks.sobiecka@gmail.com>
2023-06-04 20:39:19 +0200

    Add class AmountWidget

--- dist/js/script.js
+++ dist/js/script.js
@@ -65,7 +65,7 @@ const select = {
       thisProduct.initOrderForm();
       thisProduct.processOrder();
 
-      console.log('new Product: ', thisProduct);
+      //console.log('new Product: ', thisProduct);
     }
 
     renderInMenu(){
@@ -97,7 +97,7 @@ const select = {
 
     initAccordion(){
       const thisProduct = this;
-      console.log(thisProduct);
+      //console.log(thisProduct);
 
       /* find the clickable trigger (the element that should react to clicking) */
       //const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable); --> not needed because there is getElement
@@ -110,7 +110,7 @@ const select = {
 
         /* find active product (product that has active class) */
         const activeProduct = document.querySelector(select.all.menuProductsActive);
-        console.log (activeProduct);
+        //console.log (activeProduct);
 
         /* if there is active product and it's not thisProduct.element, remove class active from it */
         if (activeProduct != null && activeProduct != thisProduct.element){
@@ -147,25 +147,25 @@ const select = {
 
       // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
       const formData = utils.serializeFormToObject(thisProduct.form);
-      console.log('formData', formData);
+      //console.log('formData', formData);
 
       // set price to default price
       let price = thisProduct.data.price;
-      console.log(price);
+      //console.log(price);
 
       // for every category (param)...
       for(let paramId in thisProduct.data.params){
 
         // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
         const param = thisProduct.data.params[paramId];
-        console.log(paramId, param);
+        //console.log(paramId, param);
 
         // for every option in this category
         for(let optionId in param.options) {
 
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
-          console.log(optionId, option);
+          //console.log(optionId, option);
 
           //create constant for param with a name of paramId in formData that includes optionId
           const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
@@ -200,7 +200,7 @@ const select = {
             if (!option.default == true){
               //(if it's not default) add option price to price variable
               price = price + option.price
-              console.log(price)
+              //console.log(price)
             }
           } 
           // --> it's not included
@@ -220,10 +220,19 @@ const select = {
     }
   }
 
+  class AmountWidget {
+    constructor(element) {
+      const thisWidget = this;
+
+      console.log('AmountWidget: ', thisWidget);
+      console.log('constructor arguments: ', element);
+    }
+  }
+
   const app = {
     initMenu: function(){
       const thisApp = this;
-      console.log('thisApp.data:', thisApp.data);
+      //console.log('thisApp.data:', thisApp.data);
 
       for(let productData in thisApp.data.products){
         new Product(productData, thisApp.data.products[productData]);
@@ -239,11 +248,11 @@ const select = {
 
     init: function(){
       const thisApp = this;
-      console.log('*** App starting ***');
-      console.log('thisApp:', thisApp);
-      console.log('classNames:', classNames);
-      console.log('settings:', settings);
-      console.log('templates:', templates);
+      //console.log('*** App starting ***');
+      //console.log('thisApp:', thisApp);
+      //console.log('classNames:', classNames);
+      //console.log('settings:', settings);
+      //console.log('templates:', templates);
 
       thisApp.initData();
       thisApp.initMenu();
