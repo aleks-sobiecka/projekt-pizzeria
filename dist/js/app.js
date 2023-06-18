6626d6bb1cc94429f67fec2799a5ee5eeaf0a96e
Aleksandra Sobiecka <aleks.sobiecka@gmail.com>
2023-06-18 21:49:36 +0200

    Add class Booking

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,9 +1,16 @@
 import {settings, select, classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
+import Booking from './components/Booking.js';
 
 
 const app = {
+  initBooking: function(){
+    const thisApp = this;
+
+    thisApp.bookingElem = document.querySelector(select.containerOf.booking);
+    thisApp.booking = new Booking(thisApp.bookingElem);
+  },
   initPages: function(){
     const thisApp = this;
 
@@ -106,6 +113,7 @@ const app = {
       thisApp.initPages();
       thisApp.initData();
       thisApp.initCart();
+      thisApp.initBooking();
   },
 };
 
---
+++ dist/js/components/Booking.js
@@ -0,0 +1,36 @@
+import {templates, select} from './../settings.js';
+import utils from './../utils.js';
+import AmountWidget from './AmountWidget.js';
+
+class Booking{
+    constructor(element){
+        const thisBooking = this;
+
+        thisBooking.render(element);
+        thisBooking.initWidgets();
+    }
+    render(element){
+        const thisBooking = this;
+
+        thisBooking.dom = {};
+        thisBooking.dom.wrapper = element;
+
+        const generatedHTML = templates.bookingWidget();
+        const generatedDOM = utils.createDOMFromHTML(generatedHTML);
+        thisBooking.dom.wrapper.appendChild(generatedDOM);
+
+        thisBooking.dom.peopleAmount = element.querySelector(select.booking.peopleAmount);
+        thisBooking.dom.hoursAmount = element.querySelector(select.booking.hoursAmount);
+    }
+    initWidgets(){
+        const thisBooking = this;
+
+        thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
+        thisBooking.dom.peopleAmount.addEventListener('updated', function(){})
+        
+        thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
+        thisBooking.dom.hoursAmount.addEventListener('updated', function(){})
+    }
+}
+
+export default Booking;
