50fbb8fdd8fd7e0dd7c36a571c7cf68e859c31a4
Aleksandra Sobiecka <aleks.sobiecka@gmail.com>
2023-06-19 21:54:20 +0200

    Add Class HourPicker and DatePicker

--- dist/js/components/Booking.js
+++ dist/js/components/Booking.js
@@ -1,6 +1,8 @@
 import {templates, select} from './../settings.js';
 import utils from './../utils.js';
 import AmountWidget from './AmountWidget.js';
+import DatePicker from './DatePicker.js';
+import HourPicker from './HourPicker.js';
 
 class Booking{
     constructor(element){
@@ -21,6 +23,8 @@ class Booking{
 
         thisBooking.dom.peopleAmount = element.querySelector(select.booking.peopleAmount);
         thisBooking.dom.hoursAmount = element.querySelector(select.booking.hoursAmount);
+        thisBooking.dom.datePicker = element.querySelector(select.widgets.datePicker.wrapper);
+        thisBooking.dom.hourPicker = element.querySelector(select.widgets.hourPicker.wrapper);
     }
     initWidgets(){
         const thisBooking = this;
@@ -30,6 +34,12 @@ class Booking{
         
         thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
         thisBooking.dom.hoursAmount.addEventListener('updated', function(){})
+
+        thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
+        thisBooking.dom.datePicker.addEventListener('update', function(){});
+
+        thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
+        thisBooking.dom.hourPicker.addEventListener('update', function(){});
     }
 }
 
